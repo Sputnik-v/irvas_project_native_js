089980898ab3f5fde79d56d235b296ed728a58f7
Igor Tronin <igortronin2705@gmail.com>
2022-03-11 10:45:41 +0400

    Irvas-end

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,24 @@
-console.log('Все хорошо!');
+import "./slider";
+import modals from "./modules/modals";
+import tabs from './modules/tabs';
+import forms from "./modules/forms";
+import changeModalState from './modules/changeModalState';
+import timer from './modules/timer';
+import images from './modules/images';
+
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    const modalState ={};
+    let deadline = '2022-06-19';
+
+    changeModalState(modalState);
+    modals();
+    tabs('.glazing_slider' ,'.glazing_block', '.glazing_content', 'active');
+    tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
+    tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
+    forms(modalState);
+    timer('.container1', deadline);
+    images();
+
+});
